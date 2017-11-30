5b657e28cbb3e2d1d987ef36050e15c77c589f59
Sushant Shekhar <sushantshekhar20@gmail.com>
2017-11-30 17:56:26 +0530

    scss loader

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import NotesApp from './components/NotesApp'
+import './styles/styles.scss';
 
 ReactDOM.render(<NotesApp />, document.getElementById('app'));
