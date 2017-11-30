cfb76b99e895a89f5ab3ff9f27d1fd686051a200
Sushant Shekhar <sushantshekhar20@gmail.com>
2017-11-30 20:41:36 +0530

    add style

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import NotesApp from './components/NotesApp'
+import NotesApp from './components/NotesApp';
+import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 ReactDOM.render(<NotesApp />, document.getElementById('app'));
--- src/components/NotesApp.js
+++ src/components/NotesApp.js
@@ -1,98 +1,92 @@
 import React from 'react';
 import AddOption from './AddOption';
-import Options from './Options';
-import Header from './Header';
 import Action from './Action';
+import Header from './Header';
+import Options from './Options';
 import OptionModal from './OptionModal';
 
 export default class NotesApp extends React.Component {
-
   state = {
-    options  :[],
+    options: [],
     selectedOption: undefined
   };
-
   handleDeleteOptions = () => {
-    this.setState(() => ({
-        options: []
-    }));
-  }
-
+    this.setState(() => ({ options: [] }));
+  };
   handleClearSelectedOption = () => {
     this.setState(() => ({ selectedOption: undefined }));
   }
-
   handleDeleteOption = (optionToRemove) => {
-    this.setState((prevState)=> ({
-        options : prevState.options.filter((option)=>{
-          return (optionToRemove !== option);
-        })
-    }))
-  }
-
-  handlePick=()=> {
+    this.setState((prevState) => ({
+      options: prevState.options.filter((option) => optionToRemove !== option)
+    }));
+  };
+  handlePick = () => {
     const randomNum = Math.floor(Math.random() * this.state.options.length);
     const option = this.state.options[randomNum];
     this.setState(() => ({
       selectedOption: option
     }));
-  }
-  handleAddOption=(option)=>{
-    if(!option){
-      return 'Enter Task..'
+  };
+  handleAddOption = (option) => {
+    if (!option) {
+      return 'Enter valid value to add item';
+    } else if (this.state.options.indexOf(option) > -1) {
+      return 'This option already exists';
     }
-    else if (this.state.options.indexOf(option) > -1){
-        return 'Task Exists..'
-    }
+
     this.setState((prevState) => ({
-        options: prevState.options.concat([option])
+      options: prevState.options.concat(option)
     }));
-    console.log(option);
+  };
+  componentDidMount() {
+    try {
+      const json = localStorage.getItem('options');
+      const options = JSON.parse(json);
+
+      if (options) {
+        this.setState(() => ({ options }));
+      }
+    } catch (e) {
+      // Do nothing at all
+    }
   }
-
-    componentDidMount() {
-      try {
-        const json = localStorage.getItem('options');
-        const options = JSON.parse(json);
-        if(options){
-        this.setState(() => ({options}))}
-      } catch (e) {
-        console.log(e);
-      }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.options.length !== this.state.options.length) {
+      const json = JSON.stringify(this.state.options);
+      localStorage.setItem('options', json);
     }
-  
-    componentDidUpdate(prevProps,prevState) {
-      if(prevState.options.lengtn !== this.state.options.length){
-        const json = JSON.stringify(this.state.options);
-        localStorage.setItem('options',json);
-      }
-    }
-  
+  }
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
+  }
+  render() {
+    const subtitle = 'Random Task Assigner';
 
-    render() {
-      const title = 'React Notes App';
-      const subtitle = 'Random Task Assigner';
-  
-      return (
-        <div>
-
-          <Header title={title} subtitle={subtitle} />
+    return (
+      <div>
+        <Header subtitle={subtitle} />
+        <div className="container">
           <Action
             hasOptions={this.state.options.length > 0}
             handlePick={this.handlePick}
           />
-          <Options
-            options={this.state.options}
-            handleDeleteOptions={this.handleDeleteOptions}
-            handleDeleteOption={this.handleDeleteOption}
-          />
-          <AddOption handleAddOption={this.handleAddOption} />
-          <OptionModal
+          <div className="widget">
+            <Options
+              options={this.state.options}
+              handleDeleteOptions={this.handleDeleteOptions}
+              handleDeleteOption={this.handleDeleteOption}
+            />
+            <AddOption
+              handleAddOption={this.handleAddOption}
+            />
+          </div>
+        </div>
+        <OptionModal
           selectedOption={this.state.selectedOption}
           handleClearSelectedOption={this.handleClearSelectedOption}
         />
-        </div>
-      );
-    }
+      </div>
+    );
   }
-
+}
